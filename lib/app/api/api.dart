3f33f87d166ddf1d61617427980cbe3ea85d5595
opas-keat <opas.keat@gmail.com>
2023-01-16 09:12:22 +0700

    fix api call

--- lib/app/api/api.dart
+++ lib/app/api/api.dart
@@ -1,5 +1,7 @@
 class Api {
   static const baseUrl = "http://150.95.24.139:8081";
   static const baseUrlSystemLink = "http://150.95.24.139:8081/api/v1";
-  static const baseUrlLogs = "http://150.95.24.139:8084/api/v1";
+  // static const baseUrlLogs = "http://150.95.24.139:8084/api/v1";
+  static const baseUrlLogs =
+      "https://cors-anywhere.herokuapp.com/http://150.95.24.139:8084/api/v1";
 }
