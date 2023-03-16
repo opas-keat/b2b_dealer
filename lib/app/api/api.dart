e507b0ad09de387d84d08d7b9b02141022ca6528
opas-keat <opas.keat@gmail.com>
2023-03-16 12:31:29 +0700

    update api url

--- lib/app/api/api.dart
+++ lib/app/api/api.dart
@@ -1,8 +1,8 @@
 class Api {
-  static const baseUrl = "http://150.95.24.139:8081";
+  static const baseUrl = "https://pp-salesite.com";
   // static const baseUrlSystemLink = "http://150.95.24.139:8081/api/v1";
-  static const baseUrlSystemLink = "http://localhost:8081/api/v1";
-  static const baseUrlLogs = "http://150.95.24.139:8084/api/v1";
+  static const baseUrlSystemLink = "https://pp-salesite.com/api/v1";
+  static const baseUrlLogs = "https://pp-salesite.com/api/v1";
   // static const baseUrlLogs =
   //     "https://cors-anywhere.herokuapp.com/http://150.95.24.139:8084/api/v1";
 }
